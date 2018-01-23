95b0f7629a7ff413a15f9bbaca1db664fec97539
DGDaan <danielverhoef@gmail.com>
2018-01-23 17:23:52 +0100

    Nieuw
    zie ff

---
+++ gebruiker.py
@@ -0,0 +1,7 @@
+gebruiker =  (input('gebruiker ?'))
+wachtwoord = (input('wachtwoord ? '))
+
+if (gebruiker == 'Daniel' and wachtwoord == 'jeroen'):
+    print ('hiep hiep')
+else:
+    print ("Fout")
