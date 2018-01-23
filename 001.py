e0066a29545147a0f304c6feb22581d235216df9
Dudezer0 <123leefelkedag>
2018-01-23 17:34:34 +0100

    bo
    test

---
+++ 001.py
@@ -0,0 +1,8 @@
+
+kleuren = ["rood", "geel", "blauw"]
+hoeveelheid = 0
+for aantal in kleuren:
+    hoeveelheid += 2
+print("In mijn doos met lullen zitten " + str(hoeveelheid) + " mooie lullen.")
+
+    
