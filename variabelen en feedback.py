d8e3fcf1026161555feac93fa7ff4a7bda66ece6
Dudezer0 <123leefelkedag>
2018-01-24 13:22:18 +0100

    mooi man

---
+++ variabelen en feedback.py
@@ -0,0 +1,8 @@
+auto = input("wat heeft 4 wielen en is best cool om te hebben?")
+print("oh!" + auto + " is een leuk antwoord")
+verder = input("heb je verder nog wat te melden?")
+print("hahaha sowieso grappig dat je" + verder + " zegt")
+
+
+
+
